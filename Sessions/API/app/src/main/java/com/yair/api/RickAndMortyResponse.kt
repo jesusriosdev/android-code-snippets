ae5dd46902b2e72c28ca2129c1d0901a16c9d8ba
YairRuiz <72094422+YairRuiz@users.noreply.github.com>
2021-05-19 16:26:29 -0700

    Error jeje

--- Sessions/API/app/src/main/java/com/yair/api/RickAndMortyResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.yair.api
-
-import com.google.gson.annotations.SerializedName
-
-data class RickAndMortyResponse(
-    @SerializedName("status") var status:String,
-    @SerializedName("message")var images:List<String>
-    )
